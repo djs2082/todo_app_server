63b3284ce11294dd2209690ff7be79b5d8634d29
Dilip Joshi <dilipjoshis98@gmail.com>
2025-10-01 23:36:39 +0530

    updated scheam.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20251001) do
+ActiveRecord::Schema[7.1].define(version: 20251002) do
   create_table "email_templates", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "subject", null: false
